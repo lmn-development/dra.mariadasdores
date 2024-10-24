022d926721d89daa77b7023b287973d4378b4d00
lmn-development <lmndevelopment04@gmail.com>
2024-10-24 13:11:25 -0300

    refactor (develop): contact section and add responsive styling

--- src/page/home/contact.tsx
+++ src/page/home/contact.tsx
@@ -1,26 +1,29 @@
-import { Section } from "@/components/section";
 import style from "./styles/contact.module.scss";
 import { Button } from "@/components/button";
 
 export function Contact() {
   return (
-    <Section idElement="contact" classStyle={style.contactContainer}>
-      <h2 className={style.contactTitle}>Entre em Contato</h2>
-      <div className={style.contactWrapper}>
+    <section id="contact" className={style.contactSection}>
+      <div className={style.contactImage}>
+        <img src="assets/img-01.png" alt="bg" />
+      </div>
+
+      <div className={style.contactContainer}>
+        <h2 className={style.contactTitle}>Entre em Contato</h2>
         <div className={style.contactContent}>
           <div className={style.contactTexts}>
-            <p>
+            <p className={style.contactText}>
               Estou aqui para ajudar você a dar os primeiros passos em direção a
               uma vida mais equilibrada e plena. Se você está enfrentando
               desafios emocionais ou apenas deseja conversar sobre suas
               preocupações, não hesite em me procurar.
             </p>
-            <p>
+            <p className={style.contactText}>
               Cada jornada é única, e a primeira conversa pode ser um grande
               passo. Estou disponível para ouvir e apoiar você da forma que
               precisar. Sinta-se à vontade para me contatar pelo WhatsApp.
             </p>
-            <p>
+            <p className={style.contactText}>
               Lembre-se: você não está sozinho. Estou aqui para caminhar ao seu
               lado. Vamos juntos iniciar essa jornada de transformação!
             </p>
@@ -30,13 +33,11 @@ export function Contact() {
               src="assets/logo-whatsapp.png"
               alt=""
               className={style.whatsappIcon}
-              style={{ width: "20px" }}
             />
             Enviar mensagem
           </Button>
         </div>
-        <div className={style.imagem}></div>
       </div>
-    </Section>
+    </section>
   );
 }
