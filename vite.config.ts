d217e3fd306f9e7e373956a90d7ec5532cf0a23b
lmn-development <lmndevelopment04@gmail.com>
2024-10-24 14:43:40 -0300

    refactor: Update build directory in GitHub Actions workflow and Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
+const base = process.env.VITE_APP_BASE;
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/dra.mariadasdores",
+  base: base,
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src')
