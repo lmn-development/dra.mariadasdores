00b889945e11f5699125675539b9b165706b4623
lmn-development <lmndevelopment04@gmail.com>
2024-10-29 23:08:37 -0300

    feat: Add contact form with input handling and submission logic

--- src/page/home/contact.tsx
+++ src/page/home/contact.tsx
@@ -1,18 +1,27 @@
-import { openChat } from "@/utils/openChat";
-import style from "./styles/contact.module.scss";
-import { Button } from "@/components/button";
+import { openChat } from '@/utils/openChat';
+import style from './styles/contact.module.scss';
+import { Button } from '@/components/button';
+import { Section } from '@/components/section';
+import { useState } from 'react';
 
 export function Contact() {
+  const [name, setName] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
+  const [phone, setPhone] = useState<string>('');
+  const [message, setMessage] = useState<string>('');
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log(name, email, phone, message);
+  };
+
   const chat = openChat;
   return (
-    <section id="contact" className={style.contactSection}>
-      <div className={style.contactImage}>
-        <img src="assets/img-01.png" alt="bg" />
-      </div>
+    <Section classStyle={style.contactSection}>
+      <h2 className={style.contactTitle}>Entre em Contato</h2>
 
       <div className={style.contactContainer}>
-        <h2 className={style.contactTitle}>Entre em Contato</h2>
-        <div className={style.contactContent}>
+        <div className={style.contactLeft}>
           <div className={style.contactTexts}>
             <p className={style.contactText}>
               Estou aqui para ajudar você a dar os primeiros passos em direção a
@@ -25,12 +34,9 @@ export function Contact() {
               passo. Estou disponível para ouvir e apoiar você da forma que
               precisar. Sinta-se à vontade para me contatar pelo WhatsApp.
             </p>
-            <p className={style.contactText}>
-              Lembre-se: você não está sozinho. Estou aqui para caminhar ao seu
-              lado. Vamos juntos iniciar essa jornada de transformação!
-            </p>
           </div>
-          <Button className={style.contactButton} onClick={chat}>
+
+          <Button onClick={chat} className={style.contactButton}>
             <img
               src="assets/logo-whatsapp.png"
               alt=""
@@ -39,7 +45,63 @@ export function Contact() {
             Enviar mensagem
           </Button>
         </div>
+        <div className={style.lineColumn}></div>
+        <form onSubmit={handleSubmit}>
+          <label className={style.contactLabel}>
+            <span className={style.labelText}>Nome completo</span>
+            <input
+              type="text"
+              placeholder="Digite seu nome aqui"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              className={style.labelInput}
+              required
+            />
+          </label>
+
+          <label className={style.contactLabel}>
+            <span className={style.labelText}>Email</span>
+            <input
+              type="email"
+              placeholder="exemplo@email.com"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className={style.labelInput}
+              required
+            />
+          </label>
+
+          <label className={style.contactLabel}>
+            <span className={style.labelText}>Telefone</span>
+            <input
+              type="text"
+              placeholder="(11) 9xxxx-xxxx"
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+              className={style.labelInput}
+              required
+            />
+          </label>
+
+          <label className={style.contactLabel}>
+            <span className={style.labelText}>
+              Escreva uma mensagem para enviar para a psicóloga
+            </span>
+            <textarea
+              name=""
+              id=""
+              cols={30}
+              rows={10}
+              value={message}
+              className={style.labelTextarea}
+              placeholder="Mensagem"
+              onChange={(e) => setMessage(e.target.value)}
+            ></textarea>
+          </label>
+
+          <Button type="submit">Enviar</Button>
+        </form>
       </div>
-    </section>
+    </Section>
   );
 }
