d6c982ae489ee69cb485394a4239a5e151291709
lmn-development <lmndevelopment04@gmail.com>
2024-10-30 12:35:34 -0300

    fix: Comment out base configuration in vite.config.ts for clarity

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-const base = process.env.VITE_APP_BASE;
+// const base = process.env.VITE_APP_BASE;
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: base,
+  // base: "/",
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src')
